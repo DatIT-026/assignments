72764d71495abed40b1a6224267fe66b7cd273b1
FireHelper <79629118+DatIT-026@users.noreply.github.com>
2025-02-22 21:49:41 +0700

    Delete Javascript/Basic directory

--- Javascript/Basic/Day01_HelloWorld/helloworld.js
+++
@@ -1,4 +0,0 @@
-console.log("Hello, World!");
-
-let nameOfprogrammingLang = 'JavaScript'
-console.log('Welcome to 30 day of ' + nameOfprogrammingLang);
--- Javascript/Basic/Day02_Var_Const_Op_and_Data_type/variable_and_const.js
+++
@@ -1,9 +0,0 @@
-//Null in JS means an empty value
-let emptyValue = null;
-
-//Checking Data Types
-console.log(typeof "DatIT");
-console.log(typeof 9);
-console.log(typeof true); // boolean
-console.log(typeof null); // object type
-console.log(typeof undefined); //undefined
